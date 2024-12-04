72a7b28193492f589f8966461d012e72fc1df2fe
Eric Bremner <ebremner@Erics-MacBook-Pro.local>
2024-12-04 13:04:01 -0500

    Updating more initial stuff

---
+++ sass/js/games-owl.js
@@ -0,0 +1,48 @@
+(function ($, Drupal) {
+  Drupal.behaviors.gamesOwl = {
+    attach: function (context, settings) {
+      $(document).ready(function(){
+
+        $('.owl-carousel').owlCarousel({
+          loop: false,
+          nav: true,
+          rewind: true,
+          responsive:{
+            0:{
+              items:1
+            },
+            768:{
+              items:2
+            },
+            1024:{
+                items:3
+            },
+            1296:{
+                items:4
+            },
+            1600:{
+                items:5
+            }
+          }
+        })
+
+        $('.players-tournaments-block .owl-carousel').owlCarousel({
+          loop: false,
+          nav: true,
+          rewind: true,
+          responsive:{
+            0:{
+              items:1
+            },
+            768:{
+              items:2
+            },
+            1024:{
+              items:3
+            },
+          }
+        })
+      });
+    }
+  };
+})(jQuery, Drupal);
